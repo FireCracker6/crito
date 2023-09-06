1bc0e3710352c86593f715c334924ec82edc03c3
FireCracker6 <111967393+FireCracker6@users.noreply.github.com>
2023-09-06 20:11:15 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+namespace Crito
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => CreateHostBuilder(args)
+                .Build()
+                .Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureUmbracoDefaults()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                   
+                    webBuilder.UseStaticWebAssets();
+                    webBuilder.UseStartup<Startup>();
+                });
+
+
+    }
+}
